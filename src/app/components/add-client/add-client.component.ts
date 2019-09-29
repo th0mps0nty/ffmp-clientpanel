c10bb5bf119ea05aeef2e60f99f5fdcbd29e0730
Tyler Thompson <th0mps0n.ty@protonmail.com>
2019-09-29 15:00:35 -0400

    added sidebar component styling

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/Client';
 
 @Component({
-  selector: 'app-add-client',
-  templateUrl: './add-client.component.html',
-  styleUrls: ['./add-client.component.css']
+	selector: 'app-add-client',
+	templateUrl: './add-client.component.html',
+	styleUrls: [ './add-client.component.css' ]
 })
 export class AddClientComponent implements OnInit {
+	client: Client = {
+		firstName: '',
+		lastName: '',
+		email: '',
+		phone: '',
+		balance: 0
+	};
 
-  constructor() { }
+	disableBalanceOnAdd: boolean = true;
 
-  ngOnInit() {
-  }
+	constructor() {}
 
+	ngOnInit() {}
 }
