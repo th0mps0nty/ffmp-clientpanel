f3de9b3863b6597470c5df956928f2ad4d7db8c1
Tyler Thompson <th0mps0n.ty@protonmail.com>
2019-09-30 20:29:11 -0400

    added ability to edit and update balance in the client details page

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -5,34 +5,42 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Client } from 'src/app/models/Client';
 
 @Component({
-  selector: 'app-client-details',
-  templateUrl: './client-details.component.html',
-  styleUrls: ['./client-details.component.css']
+	selector: 'app-client-details',
+	templateUrl: './client-details.component.html',
+	styleUrls: [ './client-details.component.css' ]
 })
 export class ClientDetailsComponent implements OnInit {
-  id: string;
-  client: Client;
-  hasBalance = false;
-  showBalanceUpdateInput = false;
+	id: string;
+	client: Client;
+	hasBalance = false;
+	showBalanceUpdateInput = false;
 
-  constructor(
-    private clientService: ClientService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private flashMessage: FlashMessagesService
-  ) {}
+	constructor(
+		private clientService: ClientService,
+		private router: Router,
+		private route: ActivatedRoute,
+		private flashMessage: FlashMessagesService
+	) {}
 
-  ngOnInit() {
-    // Get id from url
-    this.id = this.route.snapshot.params['id'];
-    // Get client
-    this.clientService.getClient(this.id).subscribe(client => {
-      if (client != null) {
-        if (client.balance > 0) {
-          this.hasBalance = true;
-        }
-      }
-      this.client = client;
-    });
-  }
+	ngOnInit() {
+		// Get id from url
+		this.id = this.route.snapshot.params['id'];
+		// Get client
+		this.clientService.getClient(this.id).subscribe((client) => {
+			if (client != null) {
+				if (client.balance > 0) {
+					this.hasBalance = true;
+				}
+			}
+			this.client = client;
+		});
+	}
+
+	updateBalance() {
+		this.clientService.updateClient(this.client);
+		this.flashMessage.show('Balance Updated', {
+			cssClass: 'alert-success',
+			timeout: 4000
+		});
+	}
 }
